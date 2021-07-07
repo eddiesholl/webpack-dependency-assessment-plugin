bbb15b2406bcc9743bb4e76aaed9827567ebb3dd
Eddie Sholl <esholl@zendesk.com>
2021-07-07 10:22:02 +1000

    Generates list of direct deps

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class DependencyAssessmentPlugin {
     this.options = extend({
       exclude: new RegExp('$^'),
       include: new RegExp('.*'),
+      subject: new RegExp('$^'),
       failOnError: false,
       allowAsyncCycles: false,
       onDetected: false,
@@ -24,6 +25,12 @@ class DependencyAssessmentPlugin {
         if (plugin.options.onStart) {
           plugin.options.onStart({ compilation });
         }
+
+        compilation.getLogger(PluginTitle).info('hello compilation')
+
+        const graph = this.createGraphForModules(modules, plugin)
+        const output = this.formatGraph(graph)
+        compilation.getLogger(PluginTitle).info(output)
         // console.dir(modules[100].resource)
         for (let module of modules) {
           const shouldSkip = (
@@ -35,7 +42,7 @@ class DependencyAssessmentPlugin {
           if (shouldSkip) {
             continue
           }
-          console.log(module.resource)
+          compilation.getLogger(PluginTitle).log(module.resource)
 
           let maybeCyclicalPathsList = this.isCyclic(module, module, {}, compilation)
           if (maybeCyclicalPathsList) {
@@ -67,6 +74,79 @@ class DependencyAssessmentPlugin {
         }
       })
     })
+  }
+
+
+  createGraphForModules(modules, plugin) {
+    let cwd = this.options.cwd
+    const direct = {}
+
+    for (let module of modules) {
+      const shouldSkipSubject = (
+        module.resource == null ||
+        !plugin.options.subject.test(module.resource)
+      )
+      // skip the module if it matches the exclude pattern
+      if (shouldSkipSubject) {
+        continue
+      }
+
+      // Iterate over the current modules dependencies
+      for (let dependency of module.dependencies) {
+        if (
+          dependency.constructor &&
+          dependency.constructor.name === 'CommonJsSelfReferenceDependency'
+        ) {
+          continue
+        }
+
+        let depModule = null
+        depModule = dependency.module
+
+        if (!depModule) { continue }
+        // ignore dependencies that don't have an associated resource
+        if (!depModule.resource) { continue }
+
+        const shouldSkipTarget = (
+          module.resource == null ||
+          plugin.options.exclude.test(depModule.resource) ||
+          !plugin.options.include.test(depModule.resource) ||
+          plugin.options.subject.test(depModule.resource)
+        )
+        // skip the module if it matches the exclude pattern
+        if (shouldSkipTarget) {
+          continue
+        }
+
+        let subjectPath = path.relative(cwd, module.resource)
+        let subDepPath = path.relative(cwd, depModule.resource)
+        let directDepItem = direct[subDepPath]
+        if (directDepItem) {
+          if (!directDepItem.includes(subjectPath)) {
+            directDepItem.push(subjectPath)
+          }
+        } else {
+          direct[subDepPath] = [subjectPath]
+        }
+      }
+
+    }
+
+    return {
+      direct
+    }
+  }
+
+  formatGraph(graph) {
+    const directLines = Object.keys(graph.direct)
+      .sort()
+      .map(k => {
+        const subjects = graph.direct[k].sort().map(d => ` - ${d}`).join('\n')
+        return `${k}\n${subjects}`
+      })
+      .join('\n')
+
+    return `Direct:\n${directLines}`
   }
 
   isCyclic(initialModule, currentModule, seenModules, compilation) {
