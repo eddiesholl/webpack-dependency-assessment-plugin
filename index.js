5669953f4b7b843354cdc5ec2b33c3a213724558
Eddie Sholl <esholl@zendesk.com>
2021-07-07 12:22:49 +1000

    Deleting dead code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let path = require('path')
 let extend = require('util')._extend
 let chalk = require("chalk")
-let BASE_ERROR = 'Circular dependency detected:\r\n'
 let PluginTitle = 'DependencyAssessmentPlugin'
 
 class DependencyAssessmentPlugin {
@@ -30,10 +29,8 @@ class DependencyAssessmentPlugin {
         compilation.getLogger(PluginTitle).info('hello compilation')
 
         const graph = this.createGraphForModules(modules, plugin)
-        // this.outputGraph(graph, compilation)
         const output = this.formatGraph(graph)
         compilation.warnings.push(output)
-        // console.dir(modules[100].resource)
       })
     })
   }
@@ -112,78 +109,6 @@ class DependencyAssessmentPlugin {
     return `Direct dependencies:\n${directLines}`
   }
 
-  outputGraph(graph, compilation) {
-    const out = compilation.warnings
-    Object.keys(graph.direct)
-      .sort()
-      .forEach(dep => {
-        out.push(dep)
-        graph.direct[dep].sort().map(d => out.push(` - ${d}`))
-      })
-  }
-
-  isCyclic(initialModule, currentModule, seenModules, compilation) {
-    let cwd = this.options.cwd
-
-    // Add the current module to the seen modules cache
-    seenModules[currentModule.debugId] = true
-
-    // If the modules aren't associated to resources
-    // it's not possible to display how they are cyclical
-    if (!currentModule.resource || !initialModule.resource) {
-      return false
-    }
-
-    // Iterate over the current modules dependencies
-    for (let dependency of currentModule.dependencies) {
-      if (
-        dependency.constructor &&
-        dependency.constructor.name === 'CommonJsSelfReferenceDependency'
-      ) {
-        continue
-      }
-
-      let depModule = null
-      if (compilation.moduleGraph) {
-        // handle getting a module for webpack 5
-        depModule = compilation.moduleGraph.getModule(dependency)
-      } else {
-        // handle getting a module for webpack 4
-        depModule = dependency.module
-      }
-
-      if (!depModule) { continue }
-      // ignore dependencies that don't have an associated resource
-      if (!depModule.resource) { continue }
-      // ignore dependencies that are resolved asynchronously
-      if (this.options.allowAsyncCycles && dependency.weak) { continue }
-      // the dependency was resolved to the current module due to how webpack internals
-      // setup dependencies like CommonJsSelfReferenceDependency and ModuleDecoratorDependency
-      if (currentModule === depModule) {
-        continue
-      }
-
-      if (depModule.debugId in seenModules) {
-        if (depModule.debugId === initialModule.debugId) {
-          // Initial module has a circular dependency
-          return [
-            path.relative(cwd, currentModule.resource),
-            path.relative(cwd, depModule.resource)
-          ]
-        }
-        // Found a cycle, but not for this module
-        continue
-      }
-
-      let maybeCyclicalPathsList = this.isCyclic(initialModule, depModule, seenModules, compilation)
-      if (maybeCyclicalPathsList) {
-        maybeCyclicalPathsList.unshift(path.relative(cwd, currentModule.resource))
-        return maybeCyclicalPathsList
-      }
-    }
-
-    return false
-  }
 }
 
 module.exports = DependencyAssessmentPlugin
